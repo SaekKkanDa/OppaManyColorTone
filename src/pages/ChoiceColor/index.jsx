209c20d3761f61b31a3eeabb710c1459735dea41
zwonkim <k980912@naver.com>
2023-02-26 23:56:43 +0900

    Feat: 최종 결과값 저장

--- src/pages/ChoiceColor/index.jsx
+++ src/pages/ChoiceColor/index.jsx
@@ -1,11 +1,18 @@
 import { useState, useMemo, useRef, useEffect } from 'react';
-import styled from 'styled-components';
 import {Button} from '@Styles/theme'
 import { useNavigate } from 'react-router-dom';
 import { colorData } from '@Constant/colorData';
 import { useSetRecoilState, useRecoilValue } from 'recoil';
 import { CropImage, Result } from "../../recoil/app";
-
+import {
+  $Wrapper,
+  $StatusBox,
+  $StatusBar,
+  $StatusContent,
+  $Explain,
+  $ColorBox,
+  $Color
+} from './style'
 
 
 function ChoiceColor() {
@@ -16,14 +23,6 @@ function ChoiceColor() {
   
   const navigate = useNavigate()
   const selectedColor= useMemo(() => colorData[num], [num])
-
-  const nextSlide = (type) => {
-    selectedType.current.push(type)
-    setNum(num + 1)
-    if(num === 8) {
-      navigate('/result')
-    }
-  }
 
   //selectedType 배열을 객체화하여 가장 많이 선택된 값 출력
   let result = {}
@@ -53,74 +52,39 @@ function ChoiceColor() {
   //recoil에 최종 결과값 담기
   const setResult = useSetRecoilState(Result)
   const finalResult = calResult()
-  useEffect(() => {
+
+  const handleNextClick = (type) => {
+    selectedType.current.push(type)
+    setNum(num + 1)
     setResult(finalResult)
-  })
+    if(num === 8) {
+      navigate('/result')
+    }
+  }
   
   return (
-    <Wrapper>
-    <StatusBox>
-      <StatusBar width={`${(num + 1) * (100 / colorData.length)}%`}/>  
-    </StatusBox>
-    <StatusContent>{(num + 1)}/{colorData.length} 단계</StatusContent>
-    <Explain>얼굴과 제일 잘 어울리는 컬러를 선택해주세요.</Explain>
-    <ColorBox>
+    <$Wrapper>
+    <$StatusBox>
+      <$StatusBar width={`${(num + 1) * (100 / colorData.length)}%`}/>  
+    </$StatusBox>
+    <$StatusContent>{(num + 1)}/{colorData.length} 단계</$StatusContent>
+    <$Explain>얼굴과 제일 잘 어울리는 컬러를 선택해주세요.</$Explain>
+    <$ColorBox>
       {
         selectedColor.map(item => (
-          <Color 
+          <$Color 
             key={item.id}
             color={item.color}
-            onClick={() => nextSlide(item.type)}/>
+            onClick={() => handleNextClick(item.type)}/>
           )
         )
       }
-    </ColorBox> 
+    </$ColorBox> 
     <Button onClick={()=>navigate('/result')}>다음으로</Button>
-  </Wrapper>
+  </$Wrapper>
   )
 }
 
 export default ChoiceColor;
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px;
-  padding: 10px;
-`
-const StatusBox = styled.div`
-  width: 90%;
-  height: 8px;
-  border-radius: 4px;
-  background-color: #D4D4D8;
-`
 
-const StatusBar = styled.div`
-  background-color: #52525B;
-  border-radius: 4px;
-  width: ${({width}) => width};
-  height: 8px;
-`
-const StatusContent = styled.div`
-  font-size: 20px;
-  margin-bottom: 30px;
-`
-
-const Explain = styled.div`
-  font-size: 16px;
-  margin-bottom: 20px;
-`
-
-const ColorBox = styled.div`
-  display: grid;
-  grid-template-columns : 1fr 1fr;
-  width: 90%;
-  height: 500px;
-  margin-bottom: 20px;
-`
-const Color = styled.div`
-  background-color: ${({color}) => color};
-  width: 100%;
-  
-`

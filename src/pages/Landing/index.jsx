a0f117d24b35856cfdf1c5d092233a9db7996692
Jaenk-99 <jaehy0429@gmail.com>
2023-02-26 16:18:39 +0900

    시작하기 버튼 클릭 시 사진 업로드 페이지 이동

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,7 +1,22 @@
-import styled from 'styled-components';
-import theme, { Button } from '@Styles/theme';
+import { useNavigate } from 'react-router-dom';
+import {
+    LandingWrap,
+    LandingContentDiv,
+    LandingTitle,
+    LandingSubTitle,
+    LandingColorDiv,
+    LandingUserCountDiv,
+    LangingStartButton,
+    LandingPersonalColorExplanationText,
+} from './style';
 
 function LandingPage() {
+    const navigate = useNavigate();
+
+    const onClickStartButton = () => {
+        navigate('/image-upload');
+    };
+
     return (
         <>
             <LandingWrap>
@@ -12,7 +27,9 @@ function LandingPage() {
                     <LandingUserCountDiv>
                         지금까지 1,234명이 진단했어요!
                     </LandingUserCountDiv>
-                    <LangingStartButton>시작하기</LangingStartButton>
+                    <LangingStartButton onClick={onClickStartButton}>
+                        시작하기
+                    </LangingStartButton>
                     <LandingPersonalColorExplanationText>
                         퍼스널 컬러 설명 보기
                     </LandingPersonalColorExplanationText>
@@ -21,68 +38,5 @@ function LandingPage() {
         </>
     );
 }
-
-const LandingWrap = styled.div`
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
-
-const LandingContentDiv = styled.div`
-    width: 400px;
-    height: 900px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: ${theme.gray[100]};
-    border-radius: 6px;
-    border: 1px solid black;
-`;
-
-const LandingTitle = styled.div`
-    height: 66px;
-    font-size: 52px;
-    font-weight: 700;
-    margin-top: 82px;
-`;
-
-const LandingSubTitle = styled.div`
-    height: 46px;
-    font-size: 32px;
-    font-weight: 700;
-    margin-top: 14px;
-`;
-
-const LandingColorDiv = styled.div`
-    width: 284px;
-    height: 284px;
-    background-color: pink;
-    margin-top: 90px;
-`;
-
-const LandingUserCountDiv = styled.div`
-    height: 38px;
-    font-size: 20px;
-    font-weight: 400;
-    margin-top: 90px;
-`;
-
-const LangingStartButton = styled(Button)`
-    height: 88px;
-    font-size: 40px;
-    font-weight: 500;
-    cursor: pointer;
-`;
-
-const LandingPersonalColorExplanationText = styled.div`
-    margin-top: 14px;
-    font-size: 16px;
-    font-weight: 400;
-    color: ${theme.gray[400]};
-    text-decoration: underline;
-    cursor: pointer;
-`;
 
 export default LandingPage;

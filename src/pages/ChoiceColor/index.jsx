df27645c4204aa88b0a81f5f84e193ef23d25f24
zwonkim <k980912@naver.com>
2023-02-26 21:41:06 +0900

    Feat: 최종결과값 출력 함수 작성

--- src/pages/ChoiceColor/index.jsx
+++ src/pages/ChoiceColor/index.jsx
@@ -1,59 +1,70 @@
-import { useState } from 'react';
+import { useState, useMemo, useRef } from 'react';
 import styled from 'styled-components';
 import {Button} from '@Styles/theme'
 import { useNavigate } from 'react-router-dom';
-
-// 1 spring, 2 summer, 3 autumn, 4 winter
-const colorPalette = [
-  [
-    {id: 1, color: '#F9D3E6'},
-    {id: 2, color: '#E38CAF'},
-    {id: 3, color: '#DA7E81'},
-    {id: 4, color: '#DB026C'}
-  ],
-  [
-    {id: 1, color: '#FFEB80'},
-    {id: 2, color: '#F3DC40'},
-    {id: 3, color: '#E9C382'},
-    {id: 4, color: '#F7A501'}
-  ],
-  [
-    {id: 1, color: '#0078B4'},
-    {id: 2, color: '#5873A6'},
-    {id: 3, color: '#81A6A5'},
-    {id: 4, color: '#0F428A'}
-  ],
-  [
-    {id: 1, color: '#D3DD70'},
-    {id: 2, color: '#4DA73A'},
-    {id: 3, color: '#313F12'},
-    {id: 4, color: '#015836'}
-  ],
-]
+import { colorData } from '@Constant/colorData';
 
 function ChoiceColor() {
   const [num, setNum] = useState(0)
-  const [result, setResult] = useState([])
+  const selectedType = useRef([])
+  
 
   const navigate = useNavigate()
-  const nextSlide = (id) => {
-    result.push(id)
-    setResult(result)
+  const selectedColor= useMemo(() => colorData[num], [num])
+
+  const nextSlide = (type) => {
+    selectedType.current.push(type)
     setNum(num + 1)
+    if(num === 8) {
+      navigate('/result')
+    }
   }
-  // console.log(result)
+
+  //selectedType 배열을 객체화하여 가장 많이 선택된 값 출력
+  let result = {}
+  const findMax = () => {
+    selectedType.current.forEach((x) => { 
+      result[x] = (result[x] || 0) + 1; 
+    });
+    return result;
+  }
+
+  //가장 많이 선택된 type 출력
+  const calResult = () => {
+    let maxValue = -Infinity;
+    let maxKey = null;
+
+    for(let key in result) {
+      const value = result[key];
+      if(value > maxValue) {
+        maxValue = value;
+        maxKey = key
+      }
+    }
+    return maxKey
+  }
+
+  //리코일에 최종 결과값 담기
+  const finalResult = calResult()
+  
   return (
     <Wrapper>
     <StatusBox>
-      <StatusBar width={`${(num + 1) * 25}%`}/>  
+      <StatusBar width={`${(num + 1) * 11.1}%`}/>  
     </StatusBox>
-    <StatusContent>{(num + 1) * 4}/16 단계</StatusContent>
+    <StatusContent>{(num + 1)}/{colorData.length} 단계</StatusContent>
     <Explain>얼굴과 제일 잘 어울리는 컬러를 선택해주세요.</Explain>
     <ColorBox>
       {
-        colorPalette[num].map(item => <Color key={item.id} color={item.color} onClick={() => nextSlide(item.id)}/>)
+        selectedColor.map(item => (
+          <Color 
+            key={item.id}
+            color={item.color}
+            onClick={() => nextSlide(item.type)}/>
+          )
+        )
       }
-    </ColorBox>
+    </ColorBox> 
     <Button onClick={()=>navigate('/result')}>다음으로</Button>
   </Wrapper>
   )

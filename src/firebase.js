d39b2049e05ecce9592931eaa200c825237c72cd
Jaenk-99 <jaehy0429@gmail.com>
2023-02-28 13:15:47 +0900

    firebase를 통한 analytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from 'firebase/analytics';
 
 const {
   VITE_FIREBASE_API_KEY,
@@ -7,8 +8,9 @@ const {
   VITE_FIREBASE_DATABASE_URL,
   VITE_FIREBASE_PROJECT_ID,
   VITE_FIREBASE_STORAGE_BUCKET,
-  VITE_FIREBASE_MEASUREMENT_ID,
+  VITE_FIREBASE_MESSAGING_SENDER_ID,
   VITE_FIREBASE_APP_ID,
+  VITE_FIREBASE_MEASUREMENT_ID,
 } = import.meta.env;
 
 const firebaseConfig = {
@@ -17,10 +19,12 @@ const firebaseConfig = {
   databaseURL: VITE_FIREBASE_DATABASE_URL,
   projectId: VITE_FIREBASE_PROJECT_ID,
   storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: VITE_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: VITE_FIREBASE_APP_ID,
+  measurementId: VITE_FIREBASE_MEASUREMENT_ID,
 };
 const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
 const db = getFirestore(firebaseApp);
 
-export { firebaseApp, db };
+export { firebaseApp, db, analytics };

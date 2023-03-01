75fd9baccde7f58a709f34fc517adc82b0a4387d
박겸영 <yong_drg@naver.com>
2023-03-01 18:39:23 +0900

    feat(WrongAccess): 잘못된 접근 페이지 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 import MobileLayout from '@Components/Layout/MobileLayout';
@@ -5,7 +6,8 @@ import MobileLayout from '@Components/Layout/MobileLayout';
 import LandingPage from '@Pages/Landing';
 import ImageUploadPage from '@Pages/ImageUpload';
 import ResultPage from '@Pages/Result';
-import ChoiceColor from './pages/ChoiceColor/index';
+import ChoiceColor from '@Pages/ChoiceColor';
+import WrongAccessPage from '@Pages/WrongAccess';
 
 import ROUTE_PATH from '@Constant/routePath';
 
@@ -20,6 +22,7 @@ function AppRouter() {
           <Route path={imageUpload} element={<ImageUploadPage />} />
           <Route path={choiceColor} element={<ChoiceColor />} />
           <Route path={result} element={<ResultPage />} />
+          <Route path="/*" element={<WrongAccessPage />} />
         </Route>
       </Routes>
     </BrowserRouter>

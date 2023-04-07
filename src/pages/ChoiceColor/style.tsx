d7ce40994a65253853dbfcb06dbc1fbf97f80c03
박겸영 <yong_drg@naver.com>
2023-04-08 08:36:42 +0900

    feat(ChoiceColor): viewport height 실제 화면 높이와 같게 조정

--- src/pages/ChoiceColor/style.tsx
+++ src/pages/ChoiceColor/style.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const $Wrapper = styled.div`
   margin: 0 auto;
   max-width: 400px;
+  height: calc(var(--vh, 1vh) * 100);
   display: flex;
   flex-direction: column;
   align-items: center;

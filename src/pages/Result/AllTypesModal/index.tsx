765007d56ff5cc171c408358a089f2b21f06367a
박겸영 <yong_drg@naver.com>
2023-05-10 01:23:08 +0900

    feat: 퍼스널컬러 유형 전체보기 구현 중

---
+++ src/pages/Result/AllTypesModal/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { PieChart } from 'react-minimal-pie-chart';
+import { $Wrapper } from './style';
+import color from '@Data/color';
+import theme from '@Styles/theme';
+
+// interface AllTypesModalProps {
+// }
+
+const defaultLabelStyle = {
+  fontSize: '4px',
+  fontFamily: "'Noto Sans KR', sans-serif",
+};
+
+const AllTypesModal = () => {
+  return (
+    <$Wrapper>
+      <PieChart
+        data={color.map(({ name, textColor }) => ({
+          title: name,
+          color: textColor,
+          value: 1,
+        }))}
+        label={({ dataEntry }) => dataEntry.title}
+        labelStyle={(index) => ({
+          ...defaultLabelStyle,
+          fill: index > 6 ? 'white' : theme.gray[900],
+        })}
+        labelPosition={75}
+      />
+    </$Wrapper>
+  );
+};
+
+export default AllTypesModal;
---
+++ src/pages/Result/AllTypesModal/style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const $Wrapper = styled.div`
+  width: 300px;
+  margin: 0 auto;
+`;

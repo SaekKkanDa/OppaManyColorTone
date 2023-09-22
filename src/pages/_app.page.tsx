c22e74430a309c9e7ecdba3131d74d80b73a08d3
zwonkim <k980912@naver.com>
2023-09-22 12:39:50 +0900

    feat: react-intl 설정

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot, useRecoilValue } from 'recoil';
 import { ThemeProvider } from 'styled-components';
 import { useLoading } from '@Hooks/useLoading';
 import LoadingIndicator from '@Components/LoadingIndicator';
 import MobileLayout from '@Components/Layout/MobileLayout';
 import GlobalStyle from '@Styles/GlobalStyle';
 import theme from '@Styles/theme';
-
+import { IntlProvider } from 'react-intl';
+import koLanguage from '@Translations/ko.json';
+import EnLanguage from '@Translations/en.json';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 
@@ -16,6 +18,18 @@ config.autoAddCss = false;
 
 const App = ({ Component, pageProps }: AppProps) => {
   const isLoading = useLoading();
+  const [userLocale, setUserLocale] = useState('ko-KR');
+  const translationsForUsersLocale = { 'en-US': EnLanguage, ko: koLanguage }[
+    userLocale
+  ];
+  // const language = navigator.language;
+
+  useEffect(() => {
+    const locale = navigator.language;
+    setUserLocale(locale);
+  }, []);
+
+  console.log(userLocale);
 
   return (
     <>
@@ -24,14 +38,16 @@ const App = ({ Component, pageProps }: AppProps) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
-      <RecoilRoot>
-        <GlobalStyle />
-        <ThemeProvider theme={theme}>
-          <MobileLayout>
-            {isLoading ? <LoadingIndicator /> : <Component {...pageProps} />}
-          </MobileLayout>
-        </ThemeProvider>
-      </RecoilRoot>
+      <IntlProvider locale={userLocale} messages={translationsForUsersLocale}>
+        <RecoilRoot>
+          <GlobalStyle />
+          <ThemeProvider theme={theme}>
+            <MobileLayout>
+              {isLoading ? <LoadingIndicator /> : <Component {...pageProps} />}
+            </MobileLayout>
+          </ThemeProvider>
+        </RecoilRoot>
+      </IntlProvider>
     </>
   );
 };
